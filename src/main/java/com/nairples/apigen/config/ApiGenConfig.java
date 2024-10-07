97f88105114e277292ff1412e93c01d8d0c36036
mario-zannini <mariozannini@hotmail.it>
2024-10-07 08:59:48 +0200

    Created ApiGenConfig

---
+++ src/main/java/com/nairples/apigen/config/ApiGenConfig.java
@@ -0,0 +1,41 @@
+package com.nairples.apigen.config;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+@Configuration
+public class ApiGenConfig {
+	
+	@Value("${apiGen.outputDirectory}")
+	private String outputDirectory;
+
+	public String getOutputDirectory() {
+		
+		if(!StringUtils.hasLength(outputDirectory)) {
+			String userHome = System.getProperty("user.home");
+			String timestamp = getCurrentTimestamp();
+			
+			outputDirectory = userHome + 
+					File.separator + 
+					"apiGen" + 
+					File.separator + 
+					timestamp +
+					File.separator;
+		}
+		return outputDirectory;
+	}
+
+	 private String getCurrentTimestamp() {
+	        
+	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+	        return LocalDateTime.now().format(formatter);
+	    }
+	
+	
+
+}
